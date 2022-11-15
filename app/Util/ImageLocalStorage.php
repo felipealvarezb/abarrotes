1a9b8dec9c7cf9f7bbf3bf26f0395bbe582fdfa9
felipealvarezb <felipealvarezb1@gmail.com>
2022-11-15 00:24:30 -0500

    Formatting

--- app/Util/ImageLocalStorage.php
+++ app/Util/ImageLocalStorage.php
@@ -4,9 +4,12 @@ namespace App\Util;
 use App\Interfaces\ImageStorage;
 use Illuminate\Support\Facades\Storage;
 
-class ImageLocalStorage implements ImageStorage { 
-    public function store($request, $imageName){
-        if($request->hasFile('image')) {
+class ImageLocalStorage implements ImageStorage 
+{ 
+    public function store($request, $imageName)
+    {
+        if($request->hasFile('image'))
+        {
             Storage::disk('public')->put( 
                 $imageName,
                 file_get_contents($request->file('image')->getRealPath()) 
