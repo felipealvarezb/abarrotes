463ab1f33109b0a92603d76c052d5388f23fd18d
Kevin <kmloaizaa@eafit.edu.co>
2022-11-15 23:27:03 -0500

    Api proveida

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/products', 'App\Http\Controllers\Api\ProductApiController@index')->name("api.product.index");
+Route::get('/products/paginate', 'App\Http\Controllers\Api\ProductApiController@paginate')->name("api.product.paginate");
